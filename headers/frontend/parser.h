3b3be4d575c22c0bf634286278ba0ed709b9ab52
drkwitht <derektan3510@gmail.com>
2023-07-23 00:09:37 -0700

    Finished parser decls.

--- headers/frontend/parser.h
+++ headers/frontend/parser.h
@@ -8,16 +8,58 @@ typedef struct
 {
     char *src_copy_ptr;
     Lexer lexer;
+    Token previous;
     Token current;
-    Token next;
 } Parser;
 
 void parser_init(Parser *parser, char *src);
 
-Token parser_scan(Parser *parser);
+int parser_at_end(Parser *parser);
+
+Token parser_peek_back(Parser *parser);
+
+Token parser_peek_curr(Parser *parser);
 
 void parser_advance(Parser *parser);
 
 void parser_consume(Parser *parser, TokenType type, const char *err_msg);
+
+Expression parse_list(Parser *parser);
+
+Expression parse_literal(Parser *parser);
+
+Expression parse_unary(Parser *parser);
+
+Expression parse_term(Parser *parser);
+
+Expression parse_factor(Parser *parser);
+
+Expression parse_comparison(Parser *parser);
+
+Expression parse_equality(Parser *parser);
+
+Expression parse_expr(Parser *parser);
+
+Statement parse_expr_stmt(Parser *parser);
+
+Statement parse_use_stmt(Parser *parser);
+
+Statement parse_module_stmt(Parser *parser);
+
+Statement parse_block_stmt(Parser *parser);
+
+Statement parse_if_stmt(Parser *parser);
+
+Statement parse_otherwise_stmt(Parser *parser);
+
+Statement parse_while_stmt(Parser *parser);
+
+Statement parse_func_stmt(Parser *parser);
+
+Statement parse_var_decl(Parser *parser);
+
+Statement parse_stmt(Parser *parser);
+
+Statement parser_parse_all(Parser *parser);
 
 #endif
