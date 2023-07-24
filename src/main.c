07e87a9394b370d721ea3328bf42f627c5b1fc31
drkwitht <derektan3510@gmail.com>
2023-07-24 00:30:19 -0700

    Deleted old test code again.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include "frontend/lexer.h"
+#include "frontend/parser.h"
+// #include "frontend/parser.h"
 
 /**
  * @file main.c 
@@ -7,35 +7,6 @@
  * @brief Driver code for the parser with interpreter.
  * @date 2023-07-22
  */
-
-char *read_file(const char *file_path)
-{
-    // Check if file can be read.
-    if (file_path == NULL) return NULL;
-
-    FILE *freader = fopen(file_path, "r");
-
-    if (!freader) return NULL;
-
-    size_t file_len = 0;
-    char *result = NULL;
-
-    // Get file length to know how much to read as a c-string.
-    fseek(freader, 0, SEEK_END);
-    file_len = ftell(freader);
-    fseek(freader, 0, SEEK_SET);
-
-    result = malloc(file_len + 1);
-
-    // Read N bytes or characters...
-    size_t copy_count = fread(result, 1, file_len, freader);
-    result[copy_count] = '\0';
-
-    fclose(freader);
-    freader = NULL;
-
-    return result;
-}
 
 int main(int argc, char *argv[])
 {
@@ -57,31 +28,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *source = read_file(argv[2]);
+    // char *source = read_file(argv[2]);
 
-    if (!source)
-    {
-        printf("Failed to read source file %s\n", argv[2]);
-        return 1;
-    }
-
-    // Token temp;
-    // Lexer lexer;
-    // lexer_init(&lexer, source);
-
-    // do
+    // if (!source)
     // {
-    //     temp = lexer_next_token(&lexer);
+    //     printf("Failed to read source file %s\n", argv[2]);
+    //     return 1;
+    // }
 
-    //     if (temp.type == EOS) break;
-        
-    //     char *lexeme = token_as_txt(&temp, source);
-    //     printf("Token {type: %i, txt: \"%s\"}\n", temp.type, lexeme);
-
-    //     free(lexeme);
-    // } while (1);
-
-    free(source);
+    // free(source);
 
     return 0;
 }
