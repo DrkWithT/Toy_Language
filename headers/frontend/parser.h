fd8e5c39af2e4a67e206450e46b34ac980f8d875
drkwitht <derektan3510@gmail.com>
2023-07-25 21:43:47 -0700

    Added parser decls.

--- headers/frontend/parser.h
+++ headers/frontend/parser.h
@@ -3,6 +3,7 @@
 
 #include "frontend/lexer.h"
 #include "frontend/ast.h"
+#include "frontend/fileload.h"
 
 typedef struct
 {
@@ -24,42 +25,56 @@ void parser_advance(Parser *parser);
 
 void parser_consume(Parser *parser, TokenType type, const char *err_msg);
 
-Expression parse_list(Parser *parser);
+void parser_log_err(Parser *parser, size_t line, const char *msg);
 
-Expression parse_literal(Parser *parser);
+char *parser_stringify_token(Parser *parser, Token *token_ptr);
 
-Expression parse_unary(Parser *parser);
+/// SECTION: Expression Parsing
 
-Expression parse_term(Parser *parser);
+Expression *parse_primitive(Parser *parser);
 
-Expression parse_factor(Parser *parser);
+Expression *parse_literal(Parser *parser);
 
-Expression parse_comparison(Parser *parser);
+Expression *parse_call(Parser *parser);
 
-Expression parse_equality(Parser *parser);
+Expression *parse_unary(Parser *parser);
 
-Expression parse_expr(Parser *parser);
+Expression *parse_factor(Parser *parser);
 
-Statement parse_expr_stmt(Parser *parser);
+Expression *parse_term(Parser *parser);
 
-Statement parse_use_stmt(Parser *parser);
+Expression *parse_comparison(Parser *parser);
 
-Statement parse_module_stmt(Parser *parser);
+Expression *parse_equality(Parser *parser);
 
-Statement parse_block_stmt(Parser *parser);
+Expression *parse_conditions(Parser *parser);
 
-Statement parse_if_stmt(Parser *parser);
+Expression *parse_expr(Parser *parser);
 
-Statement parse_otherwise_stmt(Parser *parser);
+/// SECTION: Statement Parsing
 
-Statement parse_while_stmt(Parser *parser);
+Statement *parse_var_decl(Parser *parser);
 
-Statement parse_func_stmt(Parser *parser);
+Statement *parse_if_stmt(Parser *parser);
 
-Statement parse_var_decl(Parser *parser);
+Statement *parse_otherwise_stmt(Parser *parser);
 
-Statement parse_stmt(Parser *parser);
+Statement *parse_block_stmt(Parser *parser);
 
-Statement parser_parse_all(Parser *parser);
+Statement *parse_return_stmt(Parser *parser);
+
+Statement *parse_while_stmt(Parser *parser);
+
+Statement *parse_func_stmt(Parser *parser);
+
+Statement *parse_use_stmt(Parser *parser);
+
+Statement *parse_module_stmt(Parser *parser);
+
+Statement *parse_expr_stmt(Parser *parser);
+
+Statement *parse_stmt(Parser *parser);
+
+Script *parser_parse_all(Parser *parser, const char *script_name);
 
 #endif
